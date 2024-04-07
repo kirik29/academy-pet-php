fbb8551c84a5be3a7ada32eb49480b02084d2a91
kirik29 <kirik4245@gmail.com>
2024-04-07 14:37:56 +0300

    refactor: write add a comments

--- index.php
+++ index.php
@@ -13,39 +13,38 @@
 
 <body>
     <?php include "./components/headerCpmponent.php"; ?>
-
-    <main class="container">
-        <form method="post">
-            <?php
-            $products = [
-                ['name' => 'Футболка', 'price' => 20],
-                ['name' => 'Джинси', 'price' => 50],
-                ['name' => 'Кросівки', 'price' => 80]
-            ];
-
-            for ($i = 0; $i < count($products); $i++) {
-                echo "<label class='label'>{$products[$i]['name']} ({$products[$i]['price']} грн):</label>";
-                echo "<input type='number' name='{$products[$i]['name']}' min='0' value='0'><br>";
-            }
-            ?>
-
-            <button type="submut">Розрахувати</button>
-
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $totalCost = 0;
-                for ($i = 0; $i < count($products); $i++) {
-                    $quaontity = isset($_POST[$products[$i]['name']]) ? (int)$_POST[$products[$i]['name']] : 0;
-                    $totalCost += $products[$i]['price'] * $quaontity;
-                }
-
-                echo "<p class='total__cost'>Загальна вартість замовлення: $totalCost грн</p>";
-            }
-            ?>
-        </form>
+    <!-- yeat -->
+    <form method="post">
         <?php
+        $products = [
+            ['name' => 'Футболка', 'price' => 20],
+            ['name' => 'Джинси', 'price' => 50],
+            ['name' => 'Кросівки', 'price' => 80]
+        ];
 
+        for ($i = 0; $i < count($products); $i++) {
+            echo "<label class='label'>{$products[$i]['name']} ({$products[$i]['price']} грн):</label>";
+            echo "<input type='number' name='{$products[$i]['name']}' min='0' value='0'><br>";
+        }
         ?>
+
+        <button type="submut">Розрахувати</button>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $totalCost = 0;
+            for ($i = 0; $i < count($products); $i++) {
+                $quaontity = isset($_POST[$products[$i]['name']]) ? (int) $_POST[$products[$i]['name']] : 0;
+                $totalCost += $products[$i]['price'] * $quaontity;
+            }
+
+            echo "<p class='total__cost'>Загальна вартість замовлення: $totalCost грн</p>";
+        }
+        ?>
+    </form>
+    <?php
+
+    ?>
     </main>
 
     <?php include "./components/footerComponent.php"; ?>
